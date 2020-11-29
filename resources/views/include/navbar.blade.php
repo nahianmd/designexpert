5362bb8f2680d113285f4f2d7d898d5b0e1a79df
nahianmd <md.nahian36@gmail.com>
2020-11-29 20:17:12 +0600

    Tags

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -1,70 +1,73 @@
 
 <!--=============================================== NAVBAR ===========================================-->
 
-<nav class="navbar navbar-expand-lg site-header sticky-top d-flex justify-content-between align-items-center">
+<nav class="navbar navbar-expand-lg site-header py-lg-4 py-xl-4 sticky-top d-flex justify-content-between align-items-center">
 
-    <!-- Brand -->
-    <a class="navbar-brand brand d-md-block d-lg-block d-xl-block d-sm-none d-none " href="/">
-        Full Banger
-    </a>
+    <div class="container">
+        <!-- Brand -->
+        <a class="navbar-brand brand d-md-block d-lg-block d-xl-block d-sm-none d-none " href="/">
+            Full Banger
+        </a>
 
-    <div class="nav-search">
-        <div class="input-group search-field">
-            <input type="text" class="form-control border-0" placeholder="Search" aria-label="Recipient's username" aria-describedby="basic-addon2">
-            <div class="input-group-append">
-                <button class="btn btn-outline-secondary border-0 bg-white" type="button"><i class="far fa-search"></i></button>
+        <div class="nav-search">
+            <div class="input-group search-field">
+                <input type="text" class="form-control border-0" placeholder="Search" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary border-0 bg-white" type="button"><i class="far fa-search"></i></button>
+                </div>
             </div>
         </div>
-    </div>
 
-    <!-- large Nav -->
-    <ul class="navbar-nav d-none d-md-inline-flex d-sm-none d-lg-inline-flex d-xl-inline-flex flex-row right-nav align-items-center">
-
-        <li class="nav-item">
-            <a class="nav-link" href="#"><i class="far fa-home-alt"></i></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#"></i><span class=""><i class="far fa-comment"></i></span></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#"></i><span class=""><i class="far fa-bell"></i></span></a>
-        </li>
-
-        <li class="nav-item dropdown">
+        <!-- large Nav -->
+        <ul class="navbar-nav d-none d-md-inline-flex d-sm-none d-lg-inline-flex d-xl-inline-flex flex-row right-nav align-items-center">
             
-            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                <img src="/images/avatars/law.jpg" alt="" class="nav-avatar"> <span class="caret"></span>
-            </a>
+            <li class="nav-item pl-4 left-divider">
+                <a class="nav-link" href="#"><i class="far fa-home-alt"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"></i><span class=""><i class="far fa-comment"></i></span></a>
+            </li>
+            <li class="nav-item pr-4 mr-4 right-divider">
+                <a class="nav-link" href="#"></i><span class=""><i class="far fa-bell"></i></span></a>
+            </li>
 
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-
-                <a class="dropdown-item mb-3" href="">
-                    <i class="fal fa-user-circle mr-3"></i> Profile
+            <li class="nav-item dropdown">
+                
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    <img src="/images/avatars/law.jpg" alt="" class="nav-avatar"> <span class="caret"></span>
                 </a>
 
-                <a class="dropdown-item mb-3" href="">
-                    <i class="fal fa-cog mr-3"></i> Settings & Privacy
-                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 
-                <a class="dropdown-item mb-3" href="">
-                    <i class="fal fa-question-circle mr-3"></i> Help & Support
-                </a>
+                    <a class="dropdown-item mb-3" href="">
+                        <i class="fal fa-user-circle mr-3"></i> Profile
+                    </a>
 
-                <div class="dropdown-divider"></div>
+                    <a class="dropdown-item mb-3" href="">
+                        <i class="fal fa-cog mr-3"></i> Settings & Privacy
+                    </a>
 
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                    onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();">
-                    <i class="fal fa-sign-out-alt mr-3"></i> {{ __('Logout') }}
-                </a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
-            </div>
-        </li>
-        
-    </ul>
-</nav>
+                    <a class="dropdown-item mb-3" href="">
+                        <i class="fal fa-question-circle mr-3"></i> Help & Support
+                    </a>
+
+                    <div class="dropdown-divider"></div>
+
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+                        <i class="fal fa-sign-out-alt mr-3"></i> {{ __('Logout') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+            
+        </ul>
+    </nav>
+    </div>
+    
 
 
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,69 +18,183 @@
     Landing Page
 ======================================= --}}
 
-<div class="">
-   
-</div>
+<div class="container">
+   <!--=============================================== NAVBAR ===========================================-->
 
-{{-- <div class="container-fluid area border">
-    <div class="container text-center py-5 ">
+    <nav class="navbar navbar-expand-lg site-header py-lg-4 py-xl-4 d-flex justify-content-between align-items-center bg-white menubar">
 
-        <h1 class="home-header mb-5">Popular Areas</h1><hr> --}}
+        <!-- Brand -->
+        <span class="heading h5 d-md-block d-lg-block d-xl-block d-sm-none d-none ">
+            CHOOSE #TAGS
+        </span>
 
-        {{-- area Card--}}
-        {{-- <div class="row">
 
-            <div class="col-md-3 mt-4 border mt-3 p-0">
-                <div class="area-image-wrapper">
-                    <img class="img-fluid bg-transparent mb-3" src="/images/city/1.png" alt="Card image cap">
-                </div>
-                <div class="mt-2">
-                    <h5 class="area-title">Mumbai</h5>
-                </div>
-            </div>
-        </div> --}}
+        <ul class="nav nav-pills border py-1 rounded" id="pills-tab" role="tablist">
+            <li class="nav-item">
+              <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Posts</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Interests</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Tags</a>
+            </li>
+        </ul>
 
-        {{-- <div class="row">
-            <div class="col-md-3 mt-4">
-                <div class="card">
-                    <img class="card-img" src="/images/city/1.png" alt="Card image">
+        <a href="" class="nav-link blue-link">
+            Clear History
+        </a>
+    </nav>
+    <div class="row">
+        <div class="col-9">
+            <div class="tab-content mt-5" id="pills-tabContent">
+        
+                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">Posts</div>
+        
+                <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">Interests</div>
+        
+                <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+                    <div class="d-flex option-wrapper mb-4">
+                        <label class="option-item">
+                            <input type="checkbox" class="checkbox">
+                            <div class="option-innner">
+                                <div class="name">#Music</div>
+                            </div>
+                        </label>
+                        <label class="option-item">
+                            <input type="checkbox" class="checkbox">
+                            <div class="option-innner"> 
+                                <div class="name">#SportsClub</div>
+                            </div>
+                        </label>
+                        <label class="option-item">
+                            <input type="checkbox" class="checkbox">
+                            <div class="option-innner">
+                                <div class="name">#PhotoOfTheDay</div>
+                            </div>
+                        </label>
+                        <label class="option-item">
+                            <input type="checkbox" class="checkbox">
+                            <div class="option-innner">
+                                <div class="name">#Cute</div>
+                            </div>
+                        </label>
+                        <label class="option-item">
+                            <input type="checkbox" class="checkbox">
+                            <div class="option-innner">
+                                <div class="name">#Love</div>
+                            </div>
+                        </label>
+                        <label class="option-item">
+                            <input type="checkbox" class="checkbox">
+                            <div class="option-innner"> 
+                                <div class="name">#DancingSouls</div>
+                            </div>
+                        </label>
+                        <label class="option-item">
+                            <input type="checkbox" class="checkbox">
+                            <div class="option-innner">
+                                <div class="name">#Summer</div>
+                            </div>
+                        </label>
+                        <label class="option-item">
+                            <input type="checkbox" class="checkbox">
+                            <div class="option-innner">
+                                <div class="name">#Fashion</div>
+                            </div>
+                        </label>
+                        <label class="option-item">
+                            <input type="checkbox" class="checkbox">
+                            <div class="option-innner">
+                                <div class="name">#Art</div>
+                            </div>
+                        </label>
+                        <label class="option-item">
+                            <input type="checkbox" class="checkbox">
+                            <div class="option-innner"> 
+                                <div class="name">#Happy</div>
+                            </div>
+                        </label>
+        
+                        <label class="option-item">
+                            <input type="checkbox" class="checkbox">
+                            <div class="option-innner"> 
+                                <div class="name">#SportsClub</div>
+                            </div>
+                        </label>
+                        <label class="option-item">
+                            <input type="checkbox" class="checkbox">
+                            <div class="option-innner">
+                                <div class="name">#PhotoOfTheDay</div>
+                            </div>
+                        </label>
+                        <label class="option-item">
+                            <input type="checkbox" class="checkbox">
+                            <div class="option-innner">
+                                <div class="name">#Cute</div>
+                            </div>
+                        </label>
+                        
+        
+                    </div>
                 </div>
             </div>
-            <div class="col-md-3 mt-4">
-                <div class="card">
-                    <img class="card-img" src="/images/city/2.png" alt="Card image">
-                </div>
-            </div>
-            <div class="col-md-3 mt-4">
-                <div class="card">
-                    <img class="card-img" src="/images/city/3.png" alt="Card image">
-                </div>
-            </div>
-            <div class="col-md-3 mt-4">
-                <div class="card">
-                    <img class="card-img" src="/images/city/4.png" alt="Card image">
-                </div>
-            </div>
-            <div class="col-md-3 mt-4">
-                <div class="card">
-                    <img class="card-img" src="/images/city/5.jpg" alt="Card image">
-                </div>
-            </div>
-            <div class="col-md-3 mt-4">
-                <div class="card">
-                    <img class="card-img" src="/images/city/6.png" alt="Card image">
-                </div>
-            </div>
-            <div class="col-md-3 mt-4">
-                <div class="card">
-                    <img class="card-img" src="/images/city/7.jpg" alt="Card image">
-                </div>
-            </div>
-          </div>
         </div>
-
+        <div class="col-3 mt-5">
+            
+		
+            <a href="#">
+                <div class="storybox" 
+                    style="background: linear-gradient( rgba(34,34,34,0.78), rgba(34,34,34,0.78)), url('assets/img/posts/2.gif') no-repeat;
+                            background-size: cover;
+                            background-position: center center;
+                            -webkit-background-size: cover;
+                            -moz-background-size: cover;
+                            -o-background-size: cover;">
+                    <div class="story-body text-center">
+                        <div class=""><img class="img-circle" src="assets/img/users/10.jpg" alt="user"></div>
+                        <h4>Clifford Graham</h4>
+                        <p>2 hours ago</p>
+                    </div>		  
+                </div>
+            </a>
+            
+            <a href="#">
+                <div class="storybox" 
+                    style="background: linear-gradient( rgba(34,34,34,0.78), rgba(34,34,34,0.78)), url('assets/img/posts/2.gif') no-repeat;
+                            background-size: cover;
+                            background-position: center center;
+                            -webkit-background-size: cover;
+                            -moz-background-size: cover;
+                            -o-background-size: cover;">
+                    <div class="story-body text-center">
+                        <div class=""><img class="img-circle" src="assets/img/users/10.jpg" alt="user"></div>
+                        <h4>Clifford Graham</h4>
+                        <p>2 hours ago</p>
+                    </div>		  
+                </div>
+            </a>
+            
+            <a href="#">
+                <div class="storybox" 
+                    style="background: linear-gradient( rgba(34,34,34,0.78), rgba(34,34,34,0.78)), url('assets/img/posts/2.gif') no-repeat;
+                            background-size: cover;
+                            background-position: center center;
+                            -webkit-background-size: cover;
+                            -moz-background-size: cover;
+                            -o-background-size: cover;">
+                    <div class="story-body text-center">
+                        <div class=""><img class="img-circle" src="assets/img/users/10.jpg" alt="user"></div>
+                        <h4>Clifford Graham</h4>
+                        <p>2 hours ago</p>
+                    </div>		  
+                </div>
+            </a>
+            
+        </div>
     </div>
-</div> --}}
+    
+</div>
 
 
 
